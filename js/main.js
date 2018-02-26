69499e1903a2a6de046acceb2e961791f8de4359
apple <apple@apples-MacBook-Pro.local>
2018-02-26 19:13:33 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -5,13 +5,27 @@ $(document).ready( function() {
 
   hamburger.on("click", function(e) {
 
+  	console.log('clicked');
+
     hamburger.toggleClass("is-active");
 
-    // Do something else, like open/close menu
-    $('div.menu').toggle('.open-menu');
-    $('div#introduction').hide('#introduction');
 
-	});
+
+//     Do something else, like open/close menu
+    $('div.menu').toggle('.open-menu');
+    $('#introduction').toggle('#introduction');
+
+    if ($('#introduction').data('state') === 'hidden') {
+
+	$('#introduction').data('state','show');
+	$('#introduction').show();
+
+} else {
+	$('#introduction').data('state','show');
+	$('#introduction').data('state','hide');
+}
+
+}
 
   $('.card').fadeIn('slow');
 });

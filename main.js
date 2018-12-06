d01b694ae46460c305a46de1b868f142cc6802d1
mai xueqiao <maixueqiao@mais-MacBook-Pro.local>
2018-12-06 13:31:06 +0800

    nav

---
+++ main.js
@@ -0,0 +1,23 @@
+$(".toggle-icon").click(function () {
+  $('#nav-icon').toggleClass("pushed");
+  $(this).toggleClass("on");
+  $('.nav-container').toggleClass("on");
+  $("nav ul").toggleClass('hidden');
+});
+
+
+ // The function
+var background_image_parallax = function($object, multiplier){
+  multiplier = typeof multiplier !== 'undefined' ? multiplier : 0.5;
+    multiplier = 1 - multiplier;
+  var $doc = $(document);
+  $object.css({"background-attatchment" : "fixed"});
+    $(window).scroll(function(){
+      var from_top = $doc.scrollTop(),
+          bg_css = '0px ' +(multiplier * from_top) + 'px';
+      $object.css({"background-position" : bg_css });
+  });
+};
+
+//Just pass the jQuery object
+background_image_parallax($(".intro-page"));
